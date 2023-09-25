27bf168c5ea99a62fa60b56924d972d0ef8ce857
Bruno Feliciano <104511327+Brunofell@users.noreply.github.com>
2023-09-25 15:27:32 -0300

    wefegerwg

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    // Cria aplicação para consultar a API
+    //Menu para o usuário informar o CEP
+    //Geração de arquivo .json com os dados do endereço
+
+
+    // https://viacep.com.br/ws/82650020/json/
+
+    public static void main(String[] args) {
+
+    }
+}
