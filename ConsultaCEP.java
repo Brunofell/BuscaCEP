765ed6404abc68ce6d14b3120db1443aa16f5b6e
Bruno Feliciano <104511327+Brunofell@users.noreply.github.com>
2023-09-26 15:06:38 -0300

    classe main

--- ConsultaCEP.java
+++ ConsultaCEP.java
@@ -1,57 +1,30 @@
-import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import java.io.FileWriter;
-import java.io.IOException;
+
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
 public class ConsultaCEP {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String busca = sc.nextLine();
-        List<Cep> ceps = new ArrayList<>();
-        Gson gson = new GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                .setPrettyPrinting() // deixava em formato de json
-                .create();
+    public Endereco buscaEndereco(String cep){
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://viacep.com.br/ws/"+ cep +"/json/"))
+                .build();
 
-        while (!busca.equalsIgnoreCase("sair")){
-            System.out.print("• Digite o CEP: ");
-            String local = sc.nextLine();
-
-            if(local.equalsIgnoreCase("sair")){
-                break;
-            }
-
-            try{
-                HttpClient client = HttpClient.newHttpClient();
-                HttpRequest request = HttpRequest.newBuilder()
-                        .uri(URI.create("https://viacep.com.br/ws/"+ local +"/json/"))
-                        .build();
-                HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-                String json = response.body();
-                System.out.println(json);
-
-                FileWriter escrita  = new FileWriter("Endereco.json");
-                escrita.write(gson.toJson(json));
-                escrita.close(); // se não não faz o flush
-                System.out.println("O programa finalizou corretamente");
-
-
-            }catch (Exception e){
-                System.out.println("Erro");
-
-            }
+        try {
+            HttpResponse<String> response = HttpClient
+                    .newHttpClient()
+                    .send(request, BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), Endereco.class);
+        } catch (Exception e) {
+            throw new RuntimeException("NÃO OBTIVE ENDEREÇO A PARTIR DESTE CEP;");
         }
 
+
     }
+
 }
+
 
--- Main.java
+++ Main.java
@@ -1,12 +1,47 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    // Cria aplicação para consultar a API
+    // Cria aplicação para consultar a API ****FEITO
     //Menu para o usuário informar o CEP
     //Geração de arquivo .json com os dados do endereço
 
 
     // https://viacep.com.br/ws/82650020/json/
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        ConsultaCEP cep = new ConsultaCEP();
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("••••••••CONSULTAR CEP•••••••");
+        System.out.println("• PESQUISAR CEP    [1]     •");
+        System.out.println("• SAIR             [0]     •");
+        System.out.println("••••••••••••••••••••••••••••");
+        while (true){
+            System.out.print("• Digite uma opção: ");
+            int opcao = sc.nextInt();
+
+            if(opcao == 1){
+                try {
+                    System.out.print("• Digite o CEP: ");
+                    String selectCep = sc.next();
+                    System.out.println(" ");
+                    Endereco meuCep = cep.buscaEndereco(selectCep);
+                    System.out.println(meuCep);
+                    GerarArquivo gerador = new GerarArquivo();
+                    gerador.salvaArquivo(meuCep);
+                    break;
+                }catch (RuntimeException | IOException e){
+                    System.out.println(e.getMessage());
+                    System.out.println("Finalizando a aplicação.");
+                }
+            }if(opcao == 0){
+                System.out.println("Finalizando a aplicação.");
+                break;
+            }else{
+                System.out.println("Digite uma opção válida!");
+            }
+        }
     }
 }
+
